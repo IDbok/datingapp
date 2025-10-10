e5e2dedbf28d17dce05087b39d11e9e86cf11d97
IDbok <bokarev.fic@gmail.com>
2025-10-10 11:17:11 +0400

    add router and toasts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'members', component: MemberList, canActivate: [authGuard]},
+    {path: 'members/:id', component: MemberDetailed, canActivate: [authGuard]},
+    {path: 'lists', component: Lists, canActivate: [authGuard]},
+    {path: 'messages', component: Messages, canActivate: [authGuard]},
+    {path: '**', component: Home},
+];
