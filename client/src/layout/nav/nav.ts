eb6f1ff8beb048693667e0380dc779182eaa0589
IDbok <bokarev.fic@gmail.com>
2025-10-17 15:14:55 +0400

    add themes

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../../core/services/toast-service';
+import { themes } from '../theme';
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +11,27 @@ import { ToastService } from '../../core/services/toast-service';
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
-export class Nav {
+export class Nav implements OnInit {
   protected accountService = inject(AccountService);
   private router = inject(Router)
   private toastService = inject(ToastService);
   protected creds: any = {};
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+
+  ngOnInit() {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+  protected themes = themes;
+
+  handleSelectTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    const element = document.activeElement as HTMLElement;
+    if (element) {
+      element.blur();
+    }
+  }
 
   login() {
     this.accountService.login(this.creds).subscribe({
